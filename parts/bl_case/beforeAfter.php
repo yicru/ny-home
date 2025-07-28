86d0ce9ba948c2735bf31d6b5ff057395cb8f68d
moti <2@motisan.info>
2025-07-29 05:30:43 +0900

    施工実績セクション更新、リスト未実装

--- parts/bl_case/beforeAfter.php
+++ parts/bl_case/beforeAfter.php
@@ -1,33 +1,34 @@
 <?php
 //最後にインデントを合わせるために、バッファリングを開始する
 auto_indent_start();
+//dataの中にonce dataを入れ、
+/*
+bl_beforeAfter
+    bl_beforeAfter
+        tag
+    bl_beforeAfter_img
+        src
+        alt
+        width
+        height
+    bl_beforeAfter_txtGroup_ttl
+        txt
+    bl_beforeAfter_txtGroup_txt
+        txt
+*/
+
 ?>
 <!-- ビフォーアフター -->
-<article class="bl_beforeAfter">
-    <!-- TODO:リンクの設定 -->
-    <a href="#" class="bl_beforeAfter_link">
-        <div class="bl_beforeAfter_imgGroup">
-            <div class="bl_beforeAfter_imgGroup_group">
-                <div class="bl_beforeAfter_imgGroup_group_img">
-                    <!-- TODO:img width height設定 -->
-                    <img src="#" alt="修繕前のベランダ波板屋根" width="" height="">
-                </div>
-                <div class="bl_beforeAfter_imgGroup_group_label">BEFORE</div>
-            </div>
-            <div class="bl_beforeAfter_imgGroup_group2">
-                <div class="bl_beforeAfter_imgGroup_group2_img">
-                    <!-- TODO:img width height設定 -->
-                    <img src="#" alt="修繕後のベランダ波板屋根" width="" height="">
-                </div>
-                <div class="bl_beforeAfter_imgGroup_group2_label">AFTER</div>
-            </div>
-        </div>
-        <div class="bl_beforeAfter_txtGroup">
-            <h3 class="bl_beforeAfter_txtGroup_ttl">ベランダ波板屋根破損の修繕</h3>
-            <p class="bl_beforeAfter_txtGroup_txt">強風によりベランダの波板が破損</p>
-        </div>
-    </a>
-</article>
+<div class="bl_beforeAfter">
+    <div class="bl_beforeAfter_imgWrap">
+        <!-- TODO:img width height設定 -->
+        <img class="bl_beforeAfter_img" src="#" alt="ベランダ波板屋根破損の修繕" width="317" height="198">
+    </div>
+    <div class="bl_beforeAfter_txtGroup">
+        <h3 class="bl_beforeAfter_txtGroup_ttl">ベランダ波板屋根破損の修繕</h3>
+        <p class="bl_beforeAfter_txtGroup_txt">強風によりベランダの波板が破損</p>
+    </div>
+</div>
 <!-- end ビフォーアフター --><?php //余計な改行、インデントを防ぐためのphpタグ開始位置
 //インデントを付けて書き出し
 auto_indent_end( true , $indent, $current_indent, $indent_level);
--- parts/bl_case/case.php
+++ parts/bl_case/case.php
@@ -2,6 +2,30 @@
 //最後にインデントを合わせるために、バッファリングを開始する
 auto_indent_start();
 ?>
+<!-- 施工実績 -->
+<section class="bl_case">
+    <div class="bl_case_inner">
+        <!-- セクションヘッダー -->
+        <div class="bl_case_header">
+            <div class="el_ttlHeader">
+                <span>case study</span>
+            </div>
+            <h2 class="bl_case_header_ttl">施工実績</h2>
+        </div>
+        <!-- end セクションヘッダー -->
+
+        <!-- 実績リスト -->
+        <div class="bl_case_list"><?php //余計な改行、インデントを防ぐためのphpタグ開始位置
+            //現在のインデントの個数
+            $indent_level = 3;?>
+            <?php
+            //リスト
+            include( "parts/bl_case/beforeAfter.php" )?>
+        </div>
+        <!-- end 実績リスト -->
+    </div>
+</section>
+<!-- end 施工実績 -->
 <?php //ここにモジュールの内容 ?><?php //余計な改行、インデントを防ぐためのphpタグ開始位置
 //インデントを付けて書き出し
 auto_indent_end( true , $indent, $current_indent, $indent_level);
