0c439807dfad17d1377422a4eb67f8fc598bcb9c
moti <2@motisan.info>
2025-07-29 05:33:41 +0900

    futureセクション完成

--- parts/bl_future/future.php
+++ parts/bl_future/future.php
@@ -10,104 +10,27 @@ auto_indent_start();
                 <span>future</span>
             </div>
             <h2 class="bl_future_header_ttl">
-                <span class="bl_future_header_ttl_highlight">NY HOME</span>が選ばれる<br>
-                <span class="bl_future_header_ttl_num">3</span>つの理由
+                <div class="bl_future_header_ttl_group">
+                    <span>NY HOME</span>
+                    が選ばれる
+                </div>
+                <div class="bl_future_header_ttl_lv2">
+                    <span class="bl_future_header_ttl_num">3</span>
+                    <span>つ</span>
+                    <span>の</span>
+                    <span>理</span>
+                    <span>由</span>
+                </div>
             </h2>
         </div>
 
-        <ul class="bl_future_list">
-            <!-- ポイント1 -->
-            <li class="bl_pointCard">
-                <div class="bl_pointCard_header">
-                    <div class="bl_pointCard_header_label">POINT</div>
-                    <div class="bl_pointCard_header_num">01</div>
-                </div>
-                <div class="bl_pointCard_body">
-                    <h3 class="bl_pointCard_body_ttl">
-                        火災保険の認定金額分で<br>
-                        <span class="bl_pointCard_body_ttl_highlight">お客様負担は一切なし！</span>
-                    </h3>
-                    <div class="bl_pointCard_body_imgWrap">
-                        <img class="bl_pointCard_body_imgWrap_img" src="/img/future-img.jpg" alt="火災保険の認定金額分でお客様負担は一切なしのイラスト" width="300" height="200">
-                    </div>
-                    <div class="bl_pointCard_body_txtGroup">
-                        <p class="bl_pointCard_body_txtGroup_txt">
-                            NYHOMEは復旧義務に従い原則火災保険認定金額と認定内容に沿った工事を行います！
-                        </p>
-                        <p class="bl_pointCard_body_txtGroup_txt">
-                            保険金額以上の工事はご要望がない限り一切行いません！
-                        </p>
-                        <p class="bl_pointCard_body_txtGroup_txt">
-                            また手数料等も一切発生しませんのでご安心してご相談ください！
-                        </p>
-                    </div>
-                </div>
-            </li>
-            <!-- end ポイント1 -->
+        <ul class="bl_future_list"><?php //余計な改行、インデントを防ぐためのphpタグ開始位置
+            //現在のインデントの個数
+            $indent_level = 3;?>
 
-            <!-- ポイント2 -->
-            <li class="bl_pointCard">
-                <div class="bl_pointCard_header">
-                    <div class="bl_pointCard_header_label">POINT</div>
-                    <div class="bl_pointCard_header_num">02</div>
-                </div>
-                <div class="bl_pointCard_body">
-                    <h3 class="bl_pointCard_body_ttl">
-                        迅速な対応で<br>
-                        <span class="bl_pointCard_body_ttl_highlight">「困った」を即解決！</span>
-                    </h3>
-                    <div class="bl_pointCard_body_imgWrap">
-                        <img class="bl_pointCard_body_imgWrap_img" src="/img/future-img2.jpg" alt="迅速な対応で困ったを即解決のイラスト" width="300" height="200">
-                    </div>
-                    <div class="bl_pointCard_body_txtGroup">
-                        <p class="bl_pointCard_body_txtGroup_txt">
-                            無料相談から迅速な対応でお客様のお困りの解決に最速で対応できるよう尽力いたします！
-                        </p>
-                        <p class="bl_pointCard_body_txtGroup_txt">
-                            お家のトラブルによってはスピードが求められる工事もあります！
-                        </p>
-                        <p class="bl_pointCard_body_txtGroup_txt">
-                            少しでも被害の拡大を抑えかつ真心リフォームができるよう最適なご提案をさせていただきます！
-                        </p>
-                    </div>
-                </div>
-            </li>
-            <!-- end ポイント2 -->
-
-            <!-- ポイント3 -->
-            <li class="bl_pointCard">
-                <div class="bl_pointCard_header">
-                    <div class="bl_pointCard_header_label">POINT</div>
-                    <div class="bl_pointCard_header_num">03</div>
-                </div>
-                <div class="bl_pointCard_body">
-                    <h3 class="bl_pointCard_body_ttl">
-                        申請から工事まで必要な工程を<br>
-                        <span class="bl_pointCard_body_ttl_highlight">一貫サポート！</span>
-                    </h3>
-                    <div class="bl_pointCard_body_imgWrap">
-                        <img class="bl_pointCard_body_imgWrap_img" src="/img/future-img3.jpg" alt="申請から工事まで必要な工程を一貫サポートのイラスト" width="300" height="200">
-                    </div>
-                    <div class="bl_pointCard_body_txtGroup">
-                        <p class="bl_pointCard_body_txtGroup_txt">
-                            申請から工事まで全てNYHOMEでサポートいたします！
-                        </p>
-                        <p class="bl_pointCard_body_txtGroup_txt">
-                            安心してまずは無料相談から！
-                        </p>
-                        <p class="bl_pointCard_body_txtGroup_txt">
-                            特定業者や詐欺業者にご注意ください！
-                        </p>
-                        <p class="bl_pointCard_body_txtGroup_txt">
-                            近年火災保険を商材とした特定業者や詐欺業者が横行しています！
-                        </p>
-                        <p class="bl_pointCard_body_txtGroup_txt">
-                            十分にご注意ください！
-                        </p>
-                    </div>
-                </div>
-            </li>
-            <!-- end ポイント3 -->
+            <?php
+            //ヘッダー
+            include( "parts/bl_pointCard/pointCard.php" )?>
         </ul>
     </div>
 </section>
