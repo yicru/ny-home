ef0b40ea8f30bc0a96dde24393383c730f7f1b70
moti <2@motisan.info>
2025-07-29 05:31:31 +0900

    ダミーデータをデザインと同じに

--- parts/bl_company/company.php
+++ parts/bl_company/company.php
@@ -7,7 +7,9 @@ auto_indent_start();
     <div class="bl_company_inner">
         <!-- セクションヘッダー -->
         <div class="bl_company_header">
-            <div class="bl_company_header_tag">COMPANY</div>
+            <div class="el_ttlHeader">
+                <span>company</span>
+            </div>
             <h2 class="bl_company_header_ttl">会社概要</h2>
         </div>
         <!-- end セクションヘッダー -->
@@ -17,39 +19,47 @@ auto_indent_start();
             <dl class="bl_company_table_list">
                 <div class="bl_company_table_list_item">
                     <dt class="bl_company_table_list_item_term">企業名</dt>
-                    <dd class="bl_company_table_list_item_desc">NY HOME</dd>
+                    <dd class="bl_company_table_list_item_desc">株式会社NYHOME</dd>
                 </div>
                 <div class="bl_company_table_list_item">
                     <dt class="bl_company_table_list_item_term">代表者名</dt>
-                    <dd class="bl_company_table_list_item_desc">ダミー</dd>
+                    <dd class="bl_company_table_list_item_desc">檀上友太</dd>
                 </div>
                 <div class="bl_company_table_list_item">
                     <dt class="bl_company_table_list_item_term">住所</dt>
-                    <dd class="bl_company_table_list_item_desc">ダミー</dd>
-                </div>
-                <div class="bl_company_table_list_item">
-                    <dt class="bl_company_table_list_item_term">創業年月</dt>
-                    <dd class="bl_company_table_list_item_desc">ダミー</dd>
+                    <dd class="bl_company_table_list_item_desc">
+                        <span>
+                            ■ 本社<br>
+                            石川県金沢市本江町1-1白川ビル1階
+                        </span>
+                        <span>
+                            ■ 広島営業所<br>
+                            広島県広島市東区福田町105-1
+                        </span>
+                    </dd>
                 </div>
                 <div class="bl_company_table_list_item">
                     <dt class="bl_company_table_list_item_term">電話番号</dt>
-                    <dd class="bl_company_table_list_item_desc">ダミー</dd>
-                </div>
-                <div class="bl_company_table_list_item">
-                    <dt class="bl_company_table_list_item_term">FAX</dt>
-                    <dd class="bl_company_table_list_item_desc">ダミー</dd>
+                    <dd class="bl_company_table_list_item_desc">070-8524-5047</dd>
                 </div>
                 <div class="bl_company_table_list_item">
                     <dt class="bl_company_table_list_item_term">業務内容</dt>
-                    <dd class="bl_company_table_list_item_desc">ダミー</dd>
+                    <dd class="bl_company_table_list_item_desc">
+                        土木工事業<br>
+                        建築事業<br>
+                        屋根工事業<br>
+                        塗装工事業<br>
+                        解体工事業<br>
+                        前各号に附帯又は関連する一切の事業
+                    </dd>
                 </div>
                 <div class="bl_company_table_list_item">
                     <dt class="bl_company_table_list_item_term">営業時間</dt>
-                    <dd class="bl_company_table_list_item_desc">ダミー</dd>
+                    <dd class="bl_company_table_list_item_desc">10:00-18:00</dd>
                 </div>
                 <div class="bl_company_table_list_item">
-                    <dt class="bl_company_table_list_item_term">登録番号</dt>
-                    <dd class="bl_company_table_list_item_desc">ダミー</dd>
+                    <dt class="bl_company_table_list_item_term">定休日</dt>
+                    <dd class="bl_company_table_list_item_desc">水曜日</dd>
                 </div>
             </dl>
         </div>
