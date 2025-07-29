3b3c31b4056b495eaace5b24c5f22bdfd7cd52cf
moti <2@motisan.info>
2025-07-30 06:17:34 +0900

    worries完成

--- parts/bl_worries/worries.php
+++ parts/bl_worries/worries.php
@@ -12,85 +12,93 @@ auto_indent_start();
             </div>
             <h2 class="bl_worries_ttl">こんなお悩みありませんか？</h2>
 
-            <!-- お悩み1 -->
-            <div class="bl_worries_txtGroup">
-                <p class="bl_worries_txtGroup_txt">自然災害で家がボロボロ…</p>
+            <div class="bl_worries_fukidasi">
+                <!-- お悩み1 -->
+                <div class="bl_worries_txtGroup">
+                    <p class="bl_worries_txtGroup_txt">自然災害で家がボロボロ…</p>
+                </div>
+                <!-- end お悩み1 -->
+
+                <!-- お悩み2 -->
+                <div class="bl_worries_txtGroup bl_worries_txtGroup__rev">
+                    <p class="bl_worries_txtGroup_txt">トイレが壊れて壁と床が水浸し…</p>
+                </div>
+                <!-- end お悩み2 -->
+
+                <!-- お悩み3 -->
+                <div class="bl_worries_txtGroup">
+                    <p class="bl_worries_txtGroup_txt">掃除中に窓を壊しちゃった！！</p>
+                </div>
+                <!-- end お悩み3 -->
+
+                <!-- お悩み4 -->
+                <div class="bl_worries_txtGroup bl_worries_txtGroup__rev">
+                    <p class="bl_worries_txtGroup_txt">留守の間に盗難にあった！！</p>
+                </div>
+                <!-- end お悩み4 -->
             </div>
-            <!-- end お悩み1 -->
 
-            <!-- お悩み2 -->
-            <div class="bl_worries_txtGroup bl_worries_txtGroup__rev">
-                <p class="bl_worries_txtGroup_txt">トイレが壊れて壁と床が水浸し…</p>
-            </div>
-            <!-- end お悩み2 -->
-
-            <!-- お悩み3 -->
-            <div class="bl_worries_txtGroup">
-                <p class="bl_worries_txtGroup_txt">掃除中に窓を壊しちゃった！！</p>
-            </div>
-            <!-- end お悩み3 -->
-
-            <!-- お悩み4 -->
-            <div class="bl_worries_txtGroup bl_worries_txtGroup__rev">
-                <p class="bl_worries_txtGroup_txt">留守の間に盗難にあった！！</p>
-            </div>
-            <!-- end お悩み4 -->
-
-            <p class="bl_worries_illustText">イラストが入ります</p>
-
-            <!-- プレースホルダー画像1 -->
-            <img src="https://placehold.jp/300x200.png" alt="" class="bl_worries_img" width="" height="" aria-hidden="true">
-
-            <!-- プレースホルダー画像2 -->
-            <img src="https://placehold.jp/300x200.png" alt="" class="bl_worries_img" width="" height="" aria-hidden="true">
+            <img src="/img/worries-illlust.png" alt="" class="bl_worries_img" width="377" height="206" aria-hidden="true">
         </div>
     </div>
     <!-- end 背景グレーエリア -->
-
-    <!-- 区切り画像 -->
-    <img src="https://placehold.jp/375x42.png" alt="" class="bl_worries_separatorImg" width="375" height="42" aria-hidden="true">
 
     <!-- 背景白エリア -->
     <div class="bl_worries_group2">
         <div class="bl_worries_group2_inner">
             <div class="bl_worries_solutionTxt">
-                <p class="bl_worries_solutionTxt_para">
-                    あなたのお悩みを<br>
-                    <span class="bl_worries_solutionTxt_para_lv2">NYHOME</span>の<br>
-                    火災保険適応工事で解決！
-                </p>
+                <h2 class="bl_worries_solutionTxt_para">
+                    <span>あなたのお悩みを</span>
+                    <div class="bl_worries_solutionTxt_para_group">
+                        <span class="un_hide">NYHOME</span>
+                        <img src="/img/header-logo.jpg" alt="NYHOME" width="130" height="31" class="bl_header_logoWrap_logo">
+                        <span>の</span>
+                    </div>
+                    <div class="bl_worries_solutionTxt_para_group2">
+                        <span class="bl_worries_solutionTxt_para_lv2">火災保険適応工事で</span>解決！
+                    </div>
+                </h2>
             </div>
 
-            <p class="bl_worries_illustText">イラストが入ります</p>
-
-            <!-- プレースホルダー画像 -->
-            <img src="https://placehold.jp/300x200.png" alt="" class="bl_worries_img" width="" height="" aria-hidden="true">
+            <img src="/img/worries-illlust2.png" alt="" class="bl_worries_group2_img" width="" height="" aria-hidden="true">
 
             <h3 class="bl_worries_subTtl">
-                火災保険の申請から工事まで<br>
+                <span>火災保険の申請から工事まで</span>
                 <span class="bl_worries_subTtl_lv2">一貫して対応します！</span>
             </h3>
 
-            <div class="bl_worries_descTxt">
-                <p class="bl_worries_descTxt_para">
-                    火災保険は、自然災害による被害はもちろん、<br>
-                    <span class="bl_worries_descTxt_para_lv2">建物の破損や汚損、水漏れ、盗難など…日常生活で起こる様々なトラブルも保証の対象</span>です。
-                </p>
+            <div class="bl_worries_descTxt_group">
+                <div class="bl_worries_descTxt">
+                    <p class="bl_worries_descTxt_para">
+                        火災保険は、自然災害による被害はもちろん、<br>
+                        <span class="bl_worries_descTxt_para_lv2">
+                            建物の破損や汚損、水漏れ、盗難など…
+                        </span>
+                        日常生活で起こる様々なトラブルも保証の対象です。
+                    </p>
+                </div>
+
+                <div class="bl_worries_descTxt">
+                    <p class="bl_worries_descTxt_para">
+                        NY HOMEは、<span class="bl_worries_descTxt_para_lv2">火災保険の申請サポートから工事まで一貫して対応します！</span>
+                    </p>
+                </div>
+
+                <div class="bl_worries_descTxt">
+                    <p class="bl_worries_descTxt_para">
+                        まずは、お持ちの火災保険でどこまでの補償が受けられるのかチェック！まずはお気軽にご相談ください。
+                    </p>
+                </div>
             </div>
 
-            <div class="bl_worries_descTxt">
-                <p class="bl_worries_descTxt_para">
-                    NY HOMEは、<span class="bl_worries_descTxt_para_lv2">火災保険の申請サポートから工事まで一貫して対応します！</span>
-                </p>
-            </div>
+            <div class="bl_worries_ctaWrap"><?php //余計な改行、インデントを防ぐためのphpタグ開始位置
+                //現在のインデントの個数
+                $indent_level = 4;?>
 
-            <div class="bl_worries_descTxt">
-                <p class="bl_worries_descTxt_para">
-                    まずは、お持ちの火災保険でどこまでの補償が受けられるのかチェック！まずはお気軽にご相談ください。
-                </p>
+                <?php
+                //cta
+                include( "parts/bl_cta/cta.php" )?>
             </div>
-
-            <!-- TODO:モジュール埋め込み -->
         </div>
     </div>
     <!-- end 背景白エリア -->
