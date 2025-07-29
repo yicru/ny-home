fd989c8c5c68b08cbd611322390e5abe338dc235
moti <2@motisan.info>
2025-07-30 06:20:42 +0900

    point 1から3完成

---
+++ parts/bl_pointCard/pointCard.php
@@ -0,0 +1,105 @@
+<?php
+//最後にインデントを合わせるために、バッファリングを開始する
+auto_indent_start();
+?><!-- ポイント1 -->
+<li class="bl_pointCard">
+    <div class="bl_pointCard_headGroup">
+        <div class="bl_pointCard_headGroup_point">
+            <div class="bl_pointCard_headGroup_label">POINT</div>
+            <div class="bl_pointCard_headGroup_num">01</div>
+        </div>
+        <h3 class="bl_pointCard_headGroup_ttl">
+            <span>火災保険の認定金額分で</span>
+            <span class="bl_pointCard_headGroup_ttl_lv2">お客様負担は一切なし！</span>
+        </h3>
+    </div>
+    <div class="bl_pointCard_body">
+        <div class="bl_pointCard_body_imgWrap">
+            <img class="bl_pointCard_body_imgWrap_img" src="/img/future-img.jpg" alt="火災保険の認定金額分でお客様負担は一切なしのイラスト" width="300" height="200">
+        </div>
+        <div class="bl_pointCard_body_txtGroup">
+            <p class="bl_pointCard_body_txtGroup_txt">
+                NYHOMEは復旧義務に従い原則火災保険認定金額と認定内容に沿った工事を行います！
+            </p>
+            <p class="bl_pointCard_body_txtGroup_txt">
+                保険金額以上の工事はご要望がない限り一切行いません！
+            </p>
+            <p class="bl_pointCard_body_txtGroup_txt">
+                また手数料等も一切発生しませんのでご安心してご相談ください！
+            </p>
+        </div>
+    </div>
+</li>
+<!-- end ポイント1 -->
+
+<!-- ポイント2 -->
+<li class="bl_pointCard">
+    <div class="bl_pointCard_headGroup">
+        <div class="bl_pointCard_headGroup_point">
+            <div class="bl_pointCard_headGroup_label">POINT</div>
+            <div class="bl_pointCard_headGroup_num">02</div>
+        </div>
+        <h3 class="bl_pointCard_headGroup_ttl">
+            <span>迅速な対応で</span>
+            <span class="bl_pointCard_headGroup_ttl_lv2">「困った」を即解決！</span>
+        </h3>
+    </div>
+    <div class="bl_pointCard_body">
+        <div class="bl_pointCard_body_imgWrap">
+            <img class="bl_pointCard_body_imgWrap_img" src="/img/future-img2.jpg" alt="迅速な対応で困ったを即解決のイラスト" width="300" height="200">
+        </div>
+        <div class="bl_pointCard_body_txtGroup">
+            <p class="bl_pointCard_body_txtGroup_txt">
+                無料相談から迅速な対応でお客様のお困りの解決に最速で対応できるよう尽力いたします！
+            </p>
+            <p class="bl_pointCard_body_txtGroup_txt">
+                お家のトラブルによってはスピードが求められる工事もあります！
+            </p>
+            <p class="bl_pointCard_body_txtGroup_txt">
+                少しでも被害の拡大を抑えかつ真心リフォームができるよう最適なご提案をさせていただきます！
+            </p>
+        </div>
+    </div>
+</li>
+<!-- end ポイント2 -->
+
+<!-- ポイント3 -->
+<li class="bl_pointCard">
+    <div class="bl_pointCard_headGroup">
+        <div class="bl_pointCard_headGroup_point">
+            <div class="bl_pointCard_headGroup_label">POINT</div>
+            <div class="bl_pointCard_headGroup_num">03</div>
+        </div>
+        <h3 class="bl_pointCard_headGroup_ttl">
+            <span>申請から工事まで必要な工程を</span>
+            <span class="bl_pointCard_headGroup_ttl_lv2">一貫サポート！</span>
+        </h3>
+    </div>
+    <div class="bl_pointCard_body">
+        <div class="bl_pointCard_body_imgWrap">
+            <img class="bl_pointCard_body_imgWrap_img" src="/img/future-img3.jpg" alt="申請から工事まで必要な工程を一貫サポートのイラスト" width="300" height="200">
+        </div>
+        <div class="bl_pointCard_body_txtGroup">
+            <p class="bl_pointCard_body_txtGroup_txt">
+                申請から工事まで全てNYHOMEでサポートいたします！
+            </p>
+            <p class="bl_pointCard_body_txtGroup_txt">
+                安心してまずは無料相談から！
+            </p>
+            <p class="bl_pointCard_body_txtGroup_txt">
+                特定業者や詐欺業者にご注意ください！
+            </p>
+            <p class="bl_pointCard_body_txtGroup_txt">
+                近年火災保険を商材とした特定業者や詐欺業者が横行しています！
+            </p>
+            <p class="bl_pointCard_body_txtGroup_txt">
+                十分にご注意ください！
+            </p>
+        </div>
+    </div>
+</li>
+<!-- end ポイント3 -->
+<?php //ここにモジュールの内容 ?><?php //余計な改行、インデントを防ぐためのphpタグ開始位置
+//インデントを付けて書き出し
+auto_indent_end( true , $indent, $current_indent, $indent_level);
+?>
