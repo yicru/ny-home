5a1fae5e10321e604a41d468cb51dfde70b6391f
moti <2@motisan.info>
2025-07-30 07:03:49 +0900

    case study完成

--- parts/bl_case/beforeAfter.php
+++ parts/bl_case/beforeAfter.php
@@ -1,34 +1,36 @@
 <?php
 //最後にインデントを合わせるために、バッファリングを開始する
 auto_indent_start();
-//dataの中にonce dataを入れ、
-/*
-bl_beforeAfter
-    bl_beforeAfter
-        tag
-    bl_beforeAfter_img
-        src
-        alt
-        width
-        height
-    bl_beforeAfter_txtGroup_ttl
-        txt
-    bl_beforeAfter_txtGroup_txt
-        txt
-*/
-
 ?>
 <!-- ビフォーアフター -->
-<div class="bl_beforeAfter">
+<<?php echo htmlspecialchars($module['tag']); ?> class="bl_beforeAfter">
     <div class="bl_beforeAfter_imgWrap">
-        <!-- TODO:img width height設定 -->
-        <img class="bl_beforeAfter_img" src="#" alt="ベランダ波板屋根破損の修繕" width="317" height="198">
+        <img class="bl_beforeAfter_img"
+            src="<?php echo htmlspecialchars($module['src']); ?>"
+            alt="<?php echo htmlspecialchars($module['alt']); ?>"
+            width="<?php echo $module['width']; ?>"
+            height="<?php echo $module['height']; ?>">
     </div>
     <div class="bl_beforeAfter_txtGroup">
-        <h3 class="bl_beforeAfter_txtGroup_ttl">ベランダ波板屋根破損の修繕</h3>
-        <p class="bl_beforeAfter_txtGroup_txt">強風によりベランダの波板が破損</p>
+        <?php
+        // ttlが存在し、空でない場合のみ出力
+        if (!empty($module['ttl'])) {
+            echo '<h3 class="bl_beforeAfter_txtGroup_ttl">' . htmlspecialchars($module['ttl']) . '</h3>';
+        }
+        ?>
+        <?php
+        // txtが存在し、空でない場合のみ出力
+        if (!empty($module['txt'])) {
+            echo '<p class="bl_beforeAfter_txtGroup_txt">' . htmlspecialchars($module['txt']) . '</p>';
+        }
+
+        //この書き方でもいい、インデントを考慮していない
+        // 条件付き出力（helper関数使用）
+        //echo output_if_exists('<h3 class="bl_beforeAfter_txtGroup_ttl">%s</h3>', $module['ttl'] ?? '');
+        //echo output_if_exists('<p class="bl_beforeAfter_txtGroup_txt">%s</p>', $module['txt'] ?? '');
+        ?>
     </div>
-</div>
+</<?php echo htmlspecialchars($module['tag']); ?>>
 <!-- end ビフォーアフター --><?php //余計な改行、インデントを防ぐためのphpタグ開始位置
 //インデントを付けて書き出し
 auto_indent_end( true , $indent, $current_indent, $indent_level);
--- parts/bl_case/case.php
+++ parts/bl_case/case.php
@@ -15,13 +15,60 @@ auto_indent_start();
         <!-- end セクションヘッダー -->
 
         <!-- 実績リスト -->
-        <div class="bl_case_list"><?php //余計な改行、インデントを防ぐためのphpタグ開始位置
+        <ul class="bl_case_list"><?php //余計な改行、インデントを防ぐためのphpタグ開始位置
             //現在のインデントの個数
             $indent_level = 3;?>
             <?php
-            //リスト
-            include( "parts/bl_case/beforeAfter.php" )?>
-        </div>
+            set_include_data('bl_beforeAfter', [
+                [
+                    'tag' => 'li',
+                    'src' => '/img/case-img.jpg',
+                    'alt' => 'キッチン水漏れ：排水管からの水漏れで床材が腐敗',
+                    'width' => 317,
+                    'height' => 198,
+                    'ttl' => 'キッチン水漏れ',
+                    'txt' => '：排水管からの水漏れで床材が腐敗'
+                ],
+                [
+                    'tag' => 'li',
+                    'src' => '/img/case-img2.jpg',
+                    'alt' => 'トイレ壁水濡れ：トイレタンクからの水濡れで壁が被害',
+                    'width' => 317,
+                    'height' => 198,
+                    'ttl' => 'トイレ壁水濡れ',
+                    'txt' => '：トイレタンクからの水濡れで壁が被害'
+                ],
+                [
+                    'tag' => 'li',
+                    'src' => '/img/case-img3.jpg',
+                    'alt' => '店舗雨漏れ：強風により雨漏れが発生し店舗内設備が被害',
+                    'width' => 317,
+                    'height' => 198,
+                    'ttl' => '店舗雨漏れ',
+                    'txt' => '：強風により雨漏れが発生し店舗内設備が被害'
+                ],
+                [
+                    'tag' => 'li',
+                    'src' => '/img/case-img4.jpg',
+                    'alt' => 'ベランダ波板屋根破損：強風によりベランダの波板が破損',
+                    'width' => 317,
+                    'height' => 198,
+                    'ttl' => 'ベランダ波板屋根破損',
+                    'txt' => '：強風によりベランダの波板が破損'
+                ],
+                [
+                    'tag' => 'li',
+                    'src' => '/img/case-img5.jpg',
+                    'alt' => 'ブラインドカーテン破損：掃除中にひっかけてしまい破損',
+                    'width' => 317,
+                    'height' => 198,
+                    'ttl' => 'ブラインドカーテン破損',
+                    'txt' => '：掃除中にひっかけてしまい破損'
+                ],
+            ]);
+            // beforeAfterデータをループで呼び出し
+            render_include_loop('bl_beforeAfter', 'parts/bl_case/beforeAfter.php',3);?>
+        </ul>
         <!-- end 実績リスト -->
     </div>
 </section>
