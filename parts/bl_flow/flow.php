8c6480fb93c4b325d5b5bd947abc5e042511c725
moti <2@motisan.info>
2025-08-01 07:27:58 +0900

    flow背景のズレを修正

--- parts/bl_flow/flow.php
+++ parts/bl_flow/flow.php
@@ -16,21 +16,28 @@ auto_indent_start();
             <!-- フローステップ1 -->
             <div class="bl_flow_item">
                 <div class="bl_flow_item_icon">
-                    <img src="img/icon-phone.svg" alt="" width="" height="" aria-hidden="true">
                 </div>
                 <div class="bl_flow_item_content">
-                    <h3 class="bl_flow_item_content_ttl">01. HPからお問い合わせ</h3>
+                    <div class="bl_flow_item_content_ttlGroup">
+                        <img class="bl_flow_item_content_ttlGroup_img bl_flow_item_content_ttlGroup_img_w20h20" src="/img/flow-step.svg" alt="" aria-hidden="true" width="20" height="20">
+                        <h3 class="bl_flow_item_content_ttl">HPからお問い合わせ</h3>
+                    </div>
                     <div class="bl_flow_item_content_body">
                         <p class="bl_flow_item_content_body_txt">以下のお問い合わせ先よりお問い合わせください</p>
                         <div class="bl_flow_item_content_body_btnGroup">
                             <a href="#" class="bl_flow_item_content_body_btnGroup_btn bl_flow_item_content_body_btnGroup_btn__line">
-                                <img src="img/icon-line.svg" alt="" width="" height="" aria-hidden="true">
-                                LINEで気軽にご相談
-                                <img src="img/icon-arrow.svg" alt="" width="" height="" aria-hidden="true">
+                                <img class="bl_flow_item_content_body_btnGroup_btn_img" src="/img/line-icon.svg" alt="" width="" height="" aria-hidden="true">
+                                <div class="bl_flow_item_content_body_btnGroup_btn_group2">
+                                    <div class="bl_flow_item_content_body_btnGroup_btn_group2_group">
+                                        <span class="bl_flow_item_content_body_btnGroup_btn_lv2">LINE</span>
+                                        <span>で気軽にご相談</span>
+                                    </div>
+                                    <img class="bl_flow_item_content_body_btnGroup_btn_arrow" src="/img/arrow-white.svg" alt="" aria-hidden="true" width="12" height="12">
+                                </div>
                             </a>
-                            <a href="tel:080-123-4567" class="bl_flow_item_content_body_btnGroup_btn bl_flow_item_content_body_btnGroup_btn__phone">
-                                <img src="img/icon-phone-white.svg" alt="" width="" height="" aria-hidden="true">
-                                080-123-4567
+                            <a href="tel:070-8524-5047" class="bl_flow_item_content_body_btnGroup_btn bl_flow_item_content_body_btnGroup_btn__phone">
+                                <img class="bl_flow_item_content_body_btnGroup_btn_img" src="/img/tel-icon-white.svg" alt="" width="" height="" aria-hidden="true">
+                                <span class="bl_flow_item_content_body_btnGroup_btn_tel">070-8524-5047</span>
                                 <span class="bl_flow_item_content_body_btnGroup_btn_time">平日 9:00-17:00 営業</span>
                             </a>
                         </div>
@@ -42,10 +49,12 @@ auto_indent_start();
             <!-- フローステップ2 -->
             <div class="bl_flow_item">
                 <div class="bl_flow_item_icon">
-                    <img src="img/icon-phone.svg" alt="" width="" height="" aria-hidden="true">
                 </div>
                 <div class="bl_flow_item_content">
-                    <h3 class="bl_flow_item_content_ttl">02. 無料現地調査</h3>
+                    <div class="bl_flow_item_content_ttlGroup">
+                        <img class="bl_flow_item_content_ttlGroup_img bl_flow_item_content_ttlGroup_img_w20h20" src="/img/flow-step2.svg" alt="" aria-hidden="true" width="20" height="20">
+                        <h3 class="bl_flow_item_content_ttl">無料現地調査</h3>
+                    </div>
                     <div class="bl_flow_item_content_body">
                         <p class="bl_flow_item_content_body_txt">被害箇所が火災保険の適用箇所か、経験豊富な専門スタッフが調査します</p>
                     </div>
@@ -56,10 +65,12 @@ auto_indent_start();
             <!-- フローステップ3 -->
             <div class="bl_flow_item">
                 <div class="bl_flow_item_icon">
-                    <img src="img/icon-phone.svg" alt="" width="" height="" aria-hidden="true">
                 </div>
                 <div class="bl_flow_item_content">
-                    <h3 class="bl_flow_item_content_ttl">03. 申請書類の作成</h3>
+                    <div class="bl_flow_item_content_ttlGroup">
+                        <img class="bl_flow_item_content_ttlGroup_img bl_flow_item_content_ttlGroup_img_w20h20" src="/img/flow-step3.svg" alt="" aria-hidden="true" width="20" height="20">
+                        <h3 class="bl_flow_item_content_ttl">申請書類の作成</h3>
+                    </div>
                     <div class="bl_flow_item_content_body">
                         <p class="bl_flow_item_content_body_txt">修繕に必要な費用を無料でお見積りいたします。<br>同時に保険会社への申請に必要な書類を作成し、その書類を保険会社へ郵送していただきます。</p>
                     </div>
@@ -70,10 +81,12 @@ auto_indent_start();
             <!-- フローステップ4 -->
             <div class="bl_flow_item">
                 <div class="bl_flow_item_icon">
-                    <img src="img/icon-phone.svg" alt="" width="" height="" aria-hidden="true">
                 </div>
                 <div class="bl_flow_item_content">
-                    <h3 class="bl_flow_item_content_ttl">04. 鑑定人による現地鑑定</h3>
+                    <div class="bl_flow_item_content_ttlGroup">
+                        <img class="bl_flow_item_content_ttlGroup_img bl_flow_item_content_ttlGroup_img_w24h24" src="/img/flow-step4.svg" alt="" aria-hidden="true" width="24" height="24">
+                        <h3 class="bl_flow_item_content_ttl">鑑定人による現地鑑定</h3>
+                    </div>
                     <div class="bl_flow_item_content_body">
                         <p class="bl_flow_item_content_body_txt">保険会社の専門スタッフまたは委託された鑑定人が、建物の鑑定を行い、修繕箇所の確認を行います。</p>
                     </div>
@@ -84,10 +97,12 @@ auto_indent_start();
             <!-- フローステップ5 -->
             <div class="bl_flow_item">
                 <div class="bl_flow_item_icon">
-                    <img src="img/icon-phone.svg" alt="" width="" height="" aria-hidden="true">
                 </div>
                 <div class="bl_flow_item_content">
-                    <h3 class="bl_flow_item_content_ttl">04. 復旧工事の打ち合わせ及び工事</h3>
+                    <div class="bl_flow_item_content_ttlGroup">
+                        <img class="bl_flow_item_content_ttlGroup_img bl_flow_item_content_ttlGroup_img_w24h24" src="/img/flow-step5.svg" alt="" aria-hidden="true" width="24" height="24">
+                        <h3 class="bl_flow_item_content_ttl">復旧工事の打ち合わせ<br>及び工事</h3>
+                    </div>
                     <div class="bl_flow_item_content_body">
                         <p class="bl_flow_item_content_body_txt">弊社スタッフが、工事完了までしっかりとサポートいたします。</p>
                     </div>
